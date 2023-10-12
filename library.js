836464fc7d92c5a69eda1c5403bdb7a750a42255
BDS <113209967+BDS-001@users.noreply.github.com>
2023-10-11 21:43:56 -0400

    added table to display library

--- library.js
+++ library.js
@@ -1,9 +1,56 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(author, title, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
+
+function clearTable() {
+  const clear = document.querySelectorAll('.book')
+  clear.forEach(book => {
+    book.remove()
+  });
+}
+
+function displayLibrary() {
+  clearTable();
+
+  const library = document.querySelector('#library')
+  myLibrary.forEach(book => {
+    const row = document.createElement('tr')
+    const bookInfo = Object.values(book)
+
+
+    bookInfo.forEach(bookInfo => {
+      let bookData = document.createElement('td')
+      bookData.innerHTML = bookInfo
+      row.append(bookData)
+    });
+
+    let removeBookButtonCol = document.createElement('td')
+    removeBookButtonCol.className = 'remove-book'
+    let removeBookButton = document.createElement('button')
+    removeBookButton.innerHTML = 'REMOVE'
+    removeBookButtonCol.append(removeBookButton)
+    row.append(removeBookButtonCol)
+
+    library.append(row)
+  });
+
+}
+
+
+
+
+// testing
+let tmp = new Book('Dr. Seuss', 'The Cat in the Hat', 61, false)
+myLibrary.push(tmp)
+tmp = new Book('J. K. Rowling', 'Harry Potter and the Goblet of Fire', 746, true)
+myLibrary.push(tmp)
+displayLibrary()
