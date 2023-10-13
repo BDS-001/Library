0b21f804bbeb3bdf9fd46ba7205f3b52dd051328
BDS <113209967+BDS-001@users.noreply.github.com>
2023-10-13 15:59:10 -0400

    added read toggle

--- library.js
+++ library.js
@@ -37,24 +37,43 @@ function clearTable() {
   });
 }
 
+function toggleRead(event) {
+  const index = event.target.getAttribute('data-index');
+  const currentRead = myLibrary[index].read
+  if (currentRead === 'no') {
+    myLibrary[index].read = 'yes'
+  } else {
+    myLibrary[index].read = 'no'
+  }
+  refreshLibrary()
+}
+
 function displayLibrary() {
   const library = document.querySelector('#library')
   for(let i = 0; i < myLibrary.length; i++) {
     const book = myLibrary[i]
     const row = document.createElement('tr')
     row.className = 'book'
-    const bookInfo = Object.values(book)
+    const bookInfo = Object.keys(book)
   
 
-    bookInfo.forEach(bookInfo => {
+    bookInfo.forEach(bookKey => {
       let bookData = document.createElement('td')
-      bookData.innerHTML = bookInfo
+      bookData.className = bookKey
+      bookData.innerHTML = book[bookKey]
+      if (bookKey === 'read') {
+        const readToggle = document.createElement('button')
+        readToggle.dataset.index = i
+        readToggle.innerHTML = 'Toggle'
+        readToggle.addEventListener('click', toggleRead)
+        bookData.appendChild(readToggle)
+      }
       row.append(bookData)
     });
 
-    let removeBookButtonCol = document.createElement('td')
+    const removeBookButtonCol = document.createElement('td')
     removeBookButtonCol.className = 'remove-book'
-    let removeBookButton = document.createElement('button')
+    const removeBookButton = document.createElement('button')
     removeBookButton.innerHTML = 'REMOVE'
     removeBookButton.className = 'remove'
     removeBookButton.dataset.index = i
