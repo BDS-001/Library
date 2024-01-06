5087fa47d50332832d720c9288c31727eebb39b4
BDS <113209967+BDS-001@users.noreply.github.com>
2024-01-05 21:54:00 -0500

    started form validation function

--- library.js
+++ library.js
@@ -9,7 +9,7 @@ class Book {
   }
 
   changeRead() {
-    if (this.read === 'no') {
+    if (this.read === 'no') { 
       this.read = 'yes'
     } else {
       this.read = 'no'
@@ -136,6 +136,27 @@ function main() {
   submitFormEventListener()
   cancelFormEventListener()
 }
+
+const formValidation = (function() {
+  const title = document.getElementById('book-title')
+  const author = document.getElementById('book-author')
+  const pageCount = document.getElementById('book-pages')
+  
+  title.addEventListener("input", (event) => {
+    // Each time the user types something, we check if the
+    // form fields are valid.
+  
+    if (title.validity.valid) {
+      // In case there is an error message visible, if the field
+      // is valid, we remove the error message.
+      emailError.textContent = ""; // Reset the content of the message
+      emailError.className = "error"; // Reset the visual state of the message
+    } else {
+      // If there is still an error, show the correct error
+      showError();
+    }
+  });
+})();
 
 
 
